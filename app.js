40f9eaf0bf307995cb4a0bb497973a9465dfa160
Hamidul-1 <hamidul786cse@gmail.com>
2022-02-28 14:01:40 +0600

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,29 @@ const allPlayers = () => {
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
     fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data));
-    
+        .then(data => showPlayerDetails(data.player));
     console.log(searchValue); 
+};
+
+const showPlayerDetails = (players) => {
+    for(const player of players){
+        const parent = document.getElementById('player-container');
+
+        const div = document.createElement('div');
+        div.innerHTML = `<div class="card border p-5">
+            <div class="pro-pic">
+                <img class="w-50" src="${player.strThumb}" alt="">
+            </div>
+            <h2>Name:${player.strPlayer}</h2>
+            <h5>Country:${player.strNationality}</h5>
+            <p></p>
+            <div class="all-button">
+                <button class="btn btn-danger">Delete</button>
+                <button class="btn btn-success">Details</button>
+            </div>
+        </div>`;
+        parent.appendChild(div);
+        console.log(player);
+    }
+    
 }
